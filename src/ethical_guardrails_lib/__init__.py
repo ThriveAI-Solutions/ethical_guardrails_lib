df1b7626d80a08e1e858337131927c30ec1fb620
Kyle Root <kyle@wellconnected.co>
2025-05-01 14:12:44 -0400

    testing folder reference change

--- src/ethical_guardrails_lib/__init__.py
+++ src/ethical_guardrails_lib/__init__.py
@@ -1,3 +1,3 @@
-from ethical_guideline import get_ethical_guideline, get_ethical_guideline_why
+from ethical_guardrails_lib.ethical_guideline import get_ethical_guideline, get_ethical_guideline_why
 
 __all__ = ["get_ethical_guideline", "get_ethical_guideline_why"]
