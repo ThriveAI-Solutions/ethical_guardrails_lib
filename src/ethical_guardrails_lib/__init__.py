6c2cf9dbf2c9bc9f61dbe00a727d76b7d2b2cb67
Kyle Root <kyle@wellconnected.co>
2025-05-01 14:44:34 -0400

    idiots an idiot

--- src/ethical_guardrails_lib/__init__.py
+++ src/ethical_guardrails_lib/__init__.py
@@ -1,3 +1,3 @@
-from ethical_guardrails_lib.ethical_guideline import get_ethical_guideline, get_ethical_guideline_why
+from ethical_guardrails_lib.ethical_guideline import get_ethical_guideline, get_ethical_guideline_why, get_sample_questions, test_ethical_guard_rails
 
 __all__ = ["get_ethical_guideline", "get_ethical_guideline_why", "get_sample_questions", "test_ethical_guard_rails"]
