df48f59783c0edc6e73e3383cdba4c0f31aded94
Kyle Root <kyle@wellconnected.co>
2025-05-01 14:37:12 -0400

    exposing more functions

--- src/ethical_guardrails_lib/__init__.py
+++ src/ethical_guardrails_lib/__init__.py
@@ -1,3 +1,3 @@
 from ethical_guardrails_lib.ethical_guideline import get_ethical_guideline, get_ethical_guideline_why
 
-__all__ = ["get_ethical_guideline", "get_ethical_guideline_why"]
+__all__ = ["get_ethical_guideline", "get_ethical_guideline_why", "get_sample_questions", "test_ethical_guard_rails"]
